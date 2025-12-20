d0bfae5171ae341162c017af74f232c86867d9b8
david <adamita.david@gmail.com>
2025-12-20 20:30:38 +0200

    Added a model for permanent storage

--- analyzer/admin.py
+++ analyzer/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from .models import ResumeAnalyzer
 # Register your models here.
+
+admin.site.register(ResumeAnalyzer)
--- analyzer/models.py
+++ analyzer/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class ResumeAnalyzer(models.Model):
+
+    resume = models.TextField()
+    job_text = models.TextField()
+    match_score = models.IntegerField()
+    matched_keywords = models.JSONField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+
+        return f"Anaysis {self.id} - match score = {self.match_score}"
